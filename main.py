3c54cc7eb2c72c4a4e8ff0ac7b0b139b9bf66f7a
Sikiru Badamosi <skbadamasi@gmail.com>
2022-08-05 08:11:19 +0100

    Added while statement

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
+from html.entities import name2codepoint
+
+
 print("Welcome to python in vs code")
 def name(fname, sname):
     print("My Name Is" + " " + fname + " " + sname)
-name("Sikir", "Badamasi")    
+name("Sikir", "Badamasi"
+ 
