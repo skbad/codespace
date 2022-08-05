b8f7f28eb31376d61150d027c4fa93287d8629c6
Sikiru Badamosi <skbadamasi@gmail.com>
2022-08-05 18:19:09 +0100

    Added def statement

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-from html.entities import name2codepoint
-
-
-print("Welcome to python in vs code")
-def name(fname, sname):
-    print("My Name Is" + " " + fname + " " + sname)
-name("Sikir", "Badamasi"
+class person:
+    def getname(self):
+        print("Sikiru")
+    def getage(self):
+        print("39")
+p = person()
+p.getname()
+p.getage()    
  
