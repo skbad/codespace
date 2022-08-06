e8854b500748db1b7dc2b6b3fd97939fdefd7fa4
Sikiru Badamosi <skbadamasi@gmail.com>
2022-08-06 13:28:39 +0100

    Added myproject.py

---
+++ myproject.py
@@ -0,0 +1,27 @@
+print("Hello World!")
+
+n1 = 1.5
+n2 = 2.3
+sum = n1 + n2
+print(sum)
+
+x1 = input("Enter first number: ")
+x2 = input("Enter second number: ")
+sum = float(x1) + float(x2)
+print(sum)
+
+n = 0
+while n < 6:
+    print(n)
+    n = n + 1
+
+    x = 5
+    while x > 0:
+        print(x)
+        x = x - 1
+        print("Balstoff")
+
+        y = 0
+        while y < 5:
+            print(y)
+            y = y +1
