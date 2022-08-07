04692d80b5318b1f66c91c8869dee0a8686ff6c0
Sikiru Badamosi <skbadamasi@gmail.com>
2022-08-07 22:00:20 +0100

    Added conditional statements

--- myproject.py
+++ myproject.py
@@ -1,56 +1,6 @@
-print("Hello World!")
-
-n1 = 1.5
-n2 = 2.3
-sum = n1 + n2
-print(sum)
-
-x1 = input("Enter first number: ")
-x2 = input("Enter second number: ")
-sum = float(x1) + float(x2)
-print(sum)
-
-n = 0
-while n < 6:
-    print(n)
-    n = n + 1
-
-    x = 5
-    while x > 0:
-        print(x)
-        x = x - 1
-        print("Balstoff")
-
-        y = 0
-        while y < 5:
-            print(y)
-            y = y +1
-
-
-for i in range(0,55,5):
-    print(i)
-
-hour = 8.0
-rate = 1000.0
-pay = hour * rate
-print(pay)
-
-d = ('12')
-print(d)
-type(d)
-
-name = input("Enter What Is Your name")
-print("My Name Is ", name)
-
-name1 = input("Who are you? ")
-print("I'm ",name1)
-
-# Convert Elevator floor
-inp = input('Europe floor ')
-usf = int(inp) + 1
-print("US Floor is ", usf)
-
-xh = input('Enter hour: ')
-xr = input('Enter rate: ')
-pay = float(xh) * float(xr)
-print(pay)
+astra = "Hello Bob"
+try:
+    istra = int(astra)
+except:
+    istra = -1
+print("First", istra)        
