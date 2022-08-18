93f0c29b1d7c60c2ff7f57f20cc70edaa0e5ee2a
Sikiru Badamosi <skbadamasi@gmail.com>
2022-08-18 21:39:17 +0100

    Added bigcount program

--- myproject.py
+++ myproject.py
@@ -93,3 +93,72 @@ for value in [2, 4, 6, 7, 9, 5, 30]:
     sum = sum + value
     print(count, sum, value)
 print("After ", count, sum, sum/count )
+
+cnt = 0
+sm = 0
+print("Before ", cnt, sm)
+for val in [3, 45, 6, 75, 9, 19, 10]:
+    cnt = cnt + 1
+    sm = sm + val
+    print(cnt,":", sm,",", val)
+print("After ", cnt, sm, sm/cnt)
+
+c = 0
+for v in [2,3,4,5,6,7]:
+    c = c + 1
+    v = v + c
+    print(c, v)
+
+print("Before")
+for va in [2,4,16,45,70,60]:
+    if va > 20:
+        print('Large Number: ', va)
+print('After')
+
+big = max('Hello world')
+print(big)
+
+tiny = min('Hello')
+print(tiny)
+
+name = max("Sikiru Badamasi")
+print(name)
+
+x = min(5,10)
+print(x)
+
+y = ("Zebra", "Ostrich", "Chicken")
+p = min(y)
+print(p)
+
+
+def sex(person):
+    if person == 'man':
+        print('It is an adult male person')
+    elif person == 'female':
+        print('It is an adult female person')
+    else:
+        print('It is just a child')
+sex('boy')
+sex('man')
+sex('female')
+
+
+#Python program to count words
+
+name = input('Enter file name: ')
+handle = open(name, 'r')
+counts = dict()
+
+for line in handle:
+    words = line.split()
+    for word in words:
+        counts[word] = counts.get(word, 0) + 1
+
+bigcount = None
+bigword = None
+for word, count in list(counts.items()):
+    if bigcount is None or count > bigcount:
+        bigword = word
+        bigcount = count
+print(bigword, bigcount)
