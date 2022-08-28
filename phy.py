889a9d40afb3d66c90e1a5692b45904bf00f9d2a
Sikiru Badamosi <skbadamasi@gmail.com>
2022-08-28 20:54:21 +0100

    assignement2

--- phy.py
+++ phy.py
@@ -1,15 +1,18 @@
-def computepay(hour,rate):
-    if hour <= 40:
-        pay = hour * rate
-    elif hour > 40:
-        x = (hour - 40) * (rate * 1.5)
-        y = hour * rate
-        pay = x + y
-    return pay
-
-hrs = input('Enter Hour:')
-rte = input('Enter Rate:')
-hour = float(hrs)
-rate = float(rte)
-p = computepay(hour, rate)
-print('Pay:', p)
+# Write a program which repeatedly reads numbers until the
+#user enters “done”. Once “done” is entered, print out the total, count,
+#and average of the numbers. If the user enters anything other than a
+#number, detect their mistake using try and except and print an error
+#message and skip to the next number.
+try:
+    inp_i = input('Enter Number: ')
+    inp = int(inp_i)
+except:
+    print('Error')
+count = 0
+total = 0
+for i in inp:
+    count = count + 1
+    total = total + i
+    print(count, total, i)
+    average = total/count
+print('After',count, total, average)
