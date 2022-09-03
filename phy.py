ed794d1f36643353d16be1cdb10328bd6019aba3
Sikiru Badamosi <skbadamasi@gmail.com>
2022-09-03 22:17:31 +0100

    Added Dose Calculator

--- phy.py
+++ phy.py
@@ -1,36 +1,6 @@
-# Write a program which repeatedly reads numbers until the
-#user enters “done”. Once “done” is entered, print out the total, count,
-#and average of the numbers. If the user enters anything other than a
-#number, detect their mistake using try and except and print an error
-#message and skip to the next number.
-num = 0
-tot = 0.0
-while True:
-    sval = input('Enter Number: ')
-    if sval == 'done':
-        break
-    try:
-        fval = float(sval)
-    except:
-        print('Invalid Input')
-        continue
-        num = num + 1
-        tot = tot + fval
-print(tot, num, tot/num)
-
-fruit = 'Banana'
-letter = fruit[-2]
-print(letter)
-len(fruit)
-index = 0
-while index < len(fruit):
-    letter = fruit[index]
-    print(letter)
-    index = index + 1
-
-word = 'banana'
-count = 0
-for letter in word:
-    if letter == 'a':
-        count = count + 1
-print(count)
+biggest_num = None
+for i in [4,8,12,25,65,19,80]:
+    if biggest_num is None or i > biggest_num:
+        biggest_num = i
+    print('Loop:', i, biggest_num)
+print('Biggest Number:', biggest_num)
