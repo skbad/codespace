8bf887b72f33835498817a3c677472ba9a3ea4e1
Sikiru Badamosi <skbadamasi@gmail.com>
2022-10-09 18:17:13 +0100

    Java File

---
+++ sk/sk.java
@@ -0,0 +1,13 @@
+
+public class sk {
+
+  public static void main(String args[])
+    {
+        System.out.println("Here's our first Java program!");
+        System.out.println("Studyopedia.com - Sk Thinks");
+        System.out.println("Hello World!");
+        System.out.println("Hello");
+
+    }
+
+}
